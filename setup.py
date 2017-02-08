d58ff2d7151b80b41ef0b79bfe2c780b186f024c
Piper Merriam <pipermerriam@gmail.com>
2017-02-08 11:31:00 -0700

    0.2.0 Release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ readme = open(os.path.join(DIR, 'README.md')).read()
 
 setup(
     name='ethereum-utils',
-    version='0.1.0',
+    version='0.2.0',
     description="""Common utility functions for ethereum codebases.""",
     long_description=readme,
     author='Piper Merriam',
