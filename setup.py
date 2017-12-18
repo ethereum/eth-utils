5ebd50c73b3d62434aab49bb2b37268004971dfd
Piper Merriam <pipermerriam@gmail.com>
2017-12-18 10:09:42 -0700

    Bump version: 0.7.2 → 0.7.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import (
 setup(
     name='eth-utils',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.7.2',
+    version='0.7.3',
     description="""Common utility functions for ethereum codebases.""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
