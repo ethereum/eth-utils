fe9f73c43d00f29487a00ec1c94de2ce3dd52dbb
Jason Carver <ut96caarrs@snkmail.com>
2018-02-08 17:30:53 -0800

    drop py34, add pypy3, rename flake8->lint

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
 )
