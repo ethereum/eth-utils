e9253024d7b5e143e73c8d740b4e2692197712ea
Jason Carver <ut96caarrs@snkmail.com>
2018-02-20 09:53:35 -0800

    Bump version: 1.0.0-beta.1 → 1.0.0-beta.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import (
 setup(
     name='eth-utils',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='1.0.0-beta.1',
+    version='1.0.0-beta.2',
     description="""Common utility functions for ethereum codebases.""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
