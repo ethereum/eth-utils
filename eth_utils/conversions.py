1d8c39d7d7579c737a5e4b6db0668045de0c5deb
Paul Lange <palango@gmx.de>
2018-02-28 18:10:19 +0100

    Support bytearrays in conversion functions
    Fixes #75.

--- eth_utils/conversions.py
+++ eth_utils/conversions.py
@@ -36,7 +36,7 @@ def to_hex(primitive=None, hexstr=None, text=None):
     if is_boolean(primitive):
         return "0x1" if primitive else "0x0"
 
-    if isinstance(primitive, bytes):
+    if isinstance(primitive, (bytes, bytearray)):
         return encode_hex(primitive)
     elif is_string(primitive):
         return to_hex(text=primitive)
@@ -45,7 +45,7 @@ def to_hex(primitive=None, hexstr=None, text=None):
         return hex(primitive)
 
     raise TypeError(
-        "Unsupported type: '{0}'.  Must be one of: bool, str, bytes"
+        "Unsupported type: '{0}'.  Must be one of: bool, str, bytes, bytearray"
         "or int.".format(repr(type(primitive)))
     )
 
@@ -58,7 +58,7 @@ def to_int(primitive=None, hexstr=None, text=None):
     Values are converted this way:
 
      * primitive:
-       * bytes: big-endian integer
+       * bytes, bytearrays: big-endian integer
        * bool: True => 1, False => 0
      * hexstr: interpret hex as integer
      * text: interpret as string of digits, like '12' => 12
@@ -67,7 +67,7 @@ def to_int(primitive=None, hexstr=None, text=None):
         return int(hexstr, 16)
     elif text is not None:
         return int(text)
-    elif isinstance(primitive, bytes):
+    elif isinstance(primitive, (bytes, bytearray)):
         return big_endian_to_int(primitive)
     elif isinstance(primitive, str):
         raise TypeError("Pass in strings with keyword hexstr or text")
@@ -79,6 +79,8 @@ def to_int(primitive=None, hexstr=None, text=None):
 def to_bytes(primitive=None, hexstr=None, text=None):
     if is_boolean(primitive):
         return b'\x01' if primitive else b'\x00'
+    elif isinstance(primitive, bytearray):
+        return bytes(primitive)
     elif isinstance(primitive, bytes):
         return primitive
     elif is_integer(primitive):
@@ -89,7 +91,9 @@ def to_bytes(primitive=None, hexstr=None, text=None):
         return decode_hex(hexstr)
     elif text is not None:
         return text.encode('utf-8')
-    raise TypeError("expected an int in first arg, or keyword of hexstr or text")
+    raise TypeError(
+        "expected a bool, int, byte or bytearray in first arg, or keyword of hexstr or text"
+    )
 
 
 @validate_conversion_arguments
@@ -100,12 +104,12 @@ def to_text(primitive=None, hexstr=None, text=None):
         return text
     elif isinstance(primitive, str):
         return to_text(hexstr=primitive)
-    elif isinstance(primitive, bytes):
+    elif isinstance(primitive, (bytes, bytearray)):
         return primitive.decode('utf-8')
     elif is_integer(primitive):
         byte_encoding = int_to_big_endian(primitive)
         return to_text(byte_encoding)
-    raise TypeError("Expected an int, bytes or hexstr.")
+    raise TypeError("Expected an int, bytes, bytearray or hexstr.")
 
 
 def text_if_str(to_type, text_or_primitive):
