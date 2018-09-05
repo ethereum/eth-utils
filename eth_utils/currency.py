026127225878318a7928ea4e049e024dbde8eac2
Nick Gheorghita <nickgheorghita@gmail.com>
2018-09-05 17:49:37 -0600

    Move units into their own module and exclude from the wrath of black

--- eth_utils/currency.py
+++ eth_utils/currency.py
@@ -4,33 +4,7 @@ from decimal import localcontext
 from typing import Union
 
 from .types import is_integer, is_string
-
-
-units = {
-    "wei": decimal.Decimal("1"),  # noqa: E241
-    "kwei": decimal.Decimal("1000"),  # noqa: E241
-    "babbage": decimal.Decimal("1000"),  # noqa: E241
-    "femtoether": decimal.Decimal("1000"),  # noqa: E241
-    "mwei": decimal.Decimal("1000000"),  # noqa: E241
-    "lovelace": decimal.Decimal("1000000"),  # noqa: E241
-    "picoether": decimal.Decimal("1000000"),  # noqa: E241
-    "gwei": decimal.Decimal("1000000000"),  # noqa: E241
-    "shannon": decimal.Decimal("1000000000"),  # noqa: E241
-    "nanoether": decimal.Decimal("1000000000"),  # noqa: E241
-    "nano": decimal.Decimal("1000000000"),  # noqa: E241
-    "szabo": decimal.Decimal("1000000000000"),  # noqa: E241
-    "microether": decimal.Decimal("1000000000000"),  # noqa: E241
-    "micro": decimal.Decimal("1000000000000"),  # noqa: E241
-    "finney": decimal.Decimal("1000000000000000"),  # noqa: E241
-    "milliether": decimal.Decimal("1000000000000000"),  # noqa: E241
-    "milli": decimal.Decimal("1000000000000000"),  # noqa: E241
-    "ether": decimal.Decimal("1000000000000000000"),  # noqa: E241
-    "kether": decimal.Decimal("1000000000000000000000"),  # noqa: E241
-    "grand": decimal.Decimal("1000000000000000000000"),  # noqa: E241
-    "mether": decimal.Decimal("1000000000000000000000000"),  # noqa: E241
-    "gether": decimal.Decimal("1000000000000000000000000000"),  # noqa: E241
-    "tether": decimal.Decimal("1000000000000000000000000000000"),  # noqa: E241
-}
+from .units import units
 
 
 denoms = type("denoms", (object,), {key: int(value) for key, value in units.items()})
