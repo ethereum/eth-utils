3f8ef47885048a62337c4895c7e2be2726fdcf9b
Piper Merriam <pipermerriam@gmail.com>
2019-04-16 12:14:53 -0600

    Bump version: 1.4.1 → 1.5.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ extras_require['dev'] = (
 setup(
     name='eth-utils',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='1.4.1',
+    version='1.5.0',
     description="""Common utility functions for ethereum codebases.""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
