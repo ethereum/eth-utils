47ec81859de6802db33741a7c99a9223227bf588
Paul Lange <palango@gmx.de>
2019-06-11 10:56:08 +0200

    Use eth-typing types

--- eth_utils/typing/misc.py
+++ eth_utils/typing/misc.py
@@ -1,13 +1,23 @@
 from typing import NewType, TypeVar, Union
+import warnings
 
 from eth_typing import Address
 
+try:
+    from eth_typing import AnyAddress, ChecksumAddress, HexAddress, HexStr, Primitives
+except ImportError:
+    warnings.warn("Old version of 'eth-typing', please update.")
 
-HexAddress = NewType("HexAddress", str)  # for hex encoded addresses
-ChecksumAddress = NewType(
-    "ChecksumAddress", HexAddress
-)  # for hex addresses with checksums
-AnyAddress = TypeVar("AnyAddress", Address, HexAddress, ChecksumAddress)
-HexStr = NewType("HexStr", str)
-Primitives = Union[bytes, int, bool]
+    # for hex encoded addresses
+    HexAddress = NewType("HexAddress", str)  # type: ignore
+    ChecksumAddress = NewType(  # type: ignore
+        "ChecksumAddress", HexAddress
+    )  # for hex addresses with checksums
+    AnyAddress = TypeVar(  # type: ignore
+        "AnyAddress", Address, HexAddress, ChecksumAddress
+    )
+    HexStr = NewType("HexStr", str)  # type: ignore
+    Primitives = Union[bytes, int, bool]  # type: ignore
+
+
 T = TypeVar("T")
