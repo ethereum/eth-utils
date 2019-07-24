9fd0073467c24b77123256a523fd504f2b3950ef
Piper Merriam <pipermerriam@gmail.com>
2019-07-24 10:35:22 -0600

    PR feedback

--- eth_utils/numeric.py
+++ eth_utils/numeric.py
@@ -21,6 +21,13 @@ TValue = TypeVar("TValue", bound=TComparable)
 
 
 def clamp(lower_bound: TValue, upper_bound: TValue, value: TValue) -> TValue:
+    # The `mypy` ignore statements here are due to doing a comparison of
+    # `Union` types which isn't allowed. (per cburgdorf).  This approach was
+    # chosen over using `typing.overload` to define multiple signatures for
+    # each comparison type here since the added value of "proper" typing
+    # doesn't seem to justify the complexity of having a bunch of different
+    # signatures defined.  The external library perspective on this function
+    # should still be adequate under this approach
     if value < lower_bound:  # type: ignore
         return lower_bound
     elif value > upper_bound:  # type: ignore
