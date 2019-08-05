0b98e216d5833625bf680bc136ef80c2f183d942
Piper Merriam <pipermerriam@gmail.com>
2019-08-05 15:45:44 -0600

    PR Feedback

--- tests/logging-utils/test_DEBUG2_logging.py
+++ tests/logging-utils/test_DEBUG2_logging.py
@@ -35,7 +35,9 @@ def test_extended_debug_logger(caplog, DEBUG2_installed):
     assert record.message == "message 1"
 
 
-def test_caching_of_debug2_when_disabled(DEBUG2_installed):
+def test_caching_of_debug2_when_disabled(caplog, DEBUG2_installed):
+    caplog.set_level(DEBUG2_LEVEL_NUM, "testing")
+
     # we need a unique logger because loggers are cached
     logger = logging.getLogger("testing-{}".format(uuid.uuid4()))
 
@@ -46,17 +48,24 @@ def test_caching_of_debug2_when_disabled(DEBUG2_installed):
     # cached property should have inserted it into the dict
     assert 'show_debug2' in logger.__dict__
 
+    # sanity pre-check
+    assert len(caplog.records) == 0
+
     assert 'debug2' not in logger.__dict__
     assert logger.debug2('this should actually call the function') is None
     assert 'debug2' in logger.__dict__
     assert logger.debug2('should not do anything but hit the lambda') is None
+
+    assert len(caplog.records) == 0
 
     # now see that it always returns the value from `__dict__`
     logger.__dict__['show_debug2'] = 100
     assert logger.show_debug2 == 100
 
 
-def test_caching_of_debug2_when_enabled(DEBUG2_installed):
+def test_caching_of_debug2_when_enabled(caplog, DEBUG2_installed):
+    caplog.set_level(DEBUG2_LEVEL_NUM, "testing")
+
     # we need a unique logger because loggers are cached
     logger = logging.getLogger("testing-{}".format(uuid.uuid4()))
 
@@ -69,10 +78,15 @@ def test_caching_of_debug2_when_enabled(DEBUG2_installed):
     # cached property should have inserted it into the dict
     assert 'show_debug2' in logger.__dict__
 
+    # sanity pre-check
+    assert len(caplog.records) == 0
+
     assert 'debug2' not in logger.__dict__
     assert logger.debug2('this should actually call the function') is None
     assert 'debug2' not in logger.__dict__
     assert logger.debug2('this should still call the function') is None
+
+    assert len(caplog.records) == 2
 
     # now see that it always returns the value from `__dict__`
     logger.__dict__['show_debug2'] = 100
