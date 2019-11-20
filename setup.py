84dd58f461e067be0ac87586e2597ac3fec985ff
Nick Gheorghita <nickgheorghita@gmail.com>
2019-11-20 13:51:20 +0100

    Bump version: 1.8.0 → 1.8.0-0.1

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ extras_require['dev'] = (
 setup(
     name='eth-utils',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='1.8.0',
+    version='1.8.0-0.1',
     description="""Common utility functions for ethereum codebases.""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
