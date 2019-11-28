5e2324467a3c1972597fd158cc19a89365d1ad91
Christofon <kremsnerchristopher@gmail.com>
2019-11-28 15:53:36 +0100

    Correct Syntax  [Issue: #168]

--- eth_utils/currency.py
+++ eth_utils/currency.py
@@ -61,7 +61,7 @@ def from_wei(number: int, unit: str) -> Union[int, decimal.Decimal]:
     return result_value
 
 
-def to_wei(number: int, unit: str) -> Union[int, float, str, Decimal]:
+def to_wei(number: int, unit: str) -> Union[int, float, str, decimal.Decimal]:
     """
     Takes a number of a unit and converts it to wei.
     """
