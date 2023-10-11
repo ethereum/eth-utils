430f080437e52852e79029945f8309f8942cdbb4
Stuart Reed <stuart.reed@ethereum.org>
2023-10-11 11:06:25 -0600

    Bump version: 2.2.1 → 2.2.2

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ with open("./README.md") as readme:
 setup(
     name="eth-utils",
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version="2.2.1",
+    version="2.2.2",
     description=(
         """eth-utils: Common utility functions for python code that interacts with Ethereum"""
     ),
