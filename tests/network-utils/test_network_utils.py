b8822ef05baedbca809379d4d930a87db352b82b
Stuart Reed <stuart.reed@ethereum.org>
2023-10-20 12:44:08 -0600

    condense exception case tests into one

--- tests/network-utils/test_network_utils.py
+++ tests/network-utils/test_network_utils.py
@@ -23,21 +23,6 @@ def test_network_name_from_chain_id(chain_id, expected_name):
     assert result == expected_name
 
 
-# Use large chainId values that should never exist
-@pytest.mark.parametrize(
-    ("chain_id", "exception"),
-    [
-        (222**222, ValidationError),
-        (333**333, ValidationError),
-        (444**444, ValidationError),
-    ],
-)
-def test_network_name_from_chain_id_with_invalid_chain_id(chain_id, exception):
-    with pytest.raises(exception) as ex:
-        network_from_chain_id(chain_id)
-    assert "chain_id is not recognized" in str(ex.value)
-
-
 @pytest.mark.parametrize(
     ("chain_id", "expected_short_name"),
     [
@@ -50,21 +35,6 @@ def test_short_name_from_chain_id(chain_id, expected_short_name):
     result = short_name_from_chain_id(chain_id)
     assert isinstance(result, str)
     assert result == expected_short_name
-
-
-# Use large chainId values that should never exist
-@pytest.mark.parametrize(
-    ("chain_id", "exception"),
-    [
-        (222**222, ValidationError),
-        (333**333, ValidationError),
-        (444**444, ValidationError),
-    ],
-)
-def test_short_name_from_chain_id_with_invalid_chain_id(chain_id, exception):
-    with pytest.raises(exception) as ex:
-        network_from_chain_id(chain_id)
-    assert "chain_id is not recognized" in str(ex.value)
 
 
 @pytest.mark.parametrize(
@@ -82,16 +52,18 @@ def test_network_from_chain_id(chain_id, expected_network):
     assert result == expected_network
 
 
-# Use large chainId values that should never exist
-@pytest.mark.parametrize(
-    ("chain_id", "exception"),
-    [
-        (222**222, ValidationError),
-        (333**333, ValidationError),
-        (444**444, ValidationError),
-    ],
-)
-def test_network_from_chain_id_with_invalid_chain_id(chain_id, exception):
-    with pytest.raises(exception) as ex:
-        network_from_chain_id(chain_id)
-    assert "chain_id is not recognized" in str(ex.value)
+@pytest.mark.parametrize("invalid_chain_id", (222**222, 333**333, 444**444))
+def test_from_chain_id_utility_methods_with_invalid_chain_id(invalid_chain_id):
+    error_message = f"chain_id is not recognized: {invalid_chain_id}"
+
+    # test network_from_chain_id
+    with pytest.raises(ValidationError, match=error_message):
+        network_from_chain_id(invalid_chain_id)
+
+    # test short_name_from_chain_id
+    with pytest.raises(ValidationError, match=error_message):
+        short_name_from_chain_id(invalid_chain_id)
+
+    # test name_from_chain_id
+    with pytest.raises(ValidationError, match=error_message):
+        name_from_chain_id(invalid_chain_id)
