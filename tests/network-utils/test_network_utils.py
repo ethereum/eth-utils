c155e002478aeb810eaa931fe08d51baf87adb18
Stuart Reed <stuart.reed@ethereum.org>
2023-10-20 13:33:58 -0600

    Separate Network chainId tests from exception cases (#256)
    * Separate Network chainId tests from exception cases
    
    * Newsfragment
    
    * condense exception case tests into one

--- tests/network-utils/test_network_utils.py
+++ tests/network-utils/test_network_utils.py
@@ -14,59 +14,56 @@ from eth_utils import (
     ("chain_id", "expected_name"),
     [
         (1, "Ethereum Mainnet"),
-        (249, None),
         (2, "Expanse Network"),
         (42, "LUKSO Mainnet"),
-        (214, None),
     ],
 )
 def test_network_name_from_chain_id(chain_id, expected_name):
-    if expected_name is not None:
-        result = name_from_chain_id(chain_id)
-        assert result == expected_name
-    else:
-        with pytest.raises(ValidationError) as ex:
-            name_from_chain_id(chain_id)
-        assert "chain_id is not recognized" in str(ex.value)
+    result = name_from_chain_id(chain_id)
+    assert result == expected_name
 
 
 @pytest.mark.parametrize(
     ("chain_id", "expected_short_name"),
     [
         (1, "eth"),
-        (249, None),
         (2, "exp"),
         (42, "lukso"),
-        (214, None),
     ],
 )
 def test_short_name_from_chain_id(chain_id, expected_short_name):
-    if expected_short_name is not None:
-        result = short_name_from_chain_id(chain_id)
-        assert isinstance(result, str)
-        assert result == expected_short_name
-    else:
-        with pytest.raises(ValidationError) as ex:
-            short_name_from_chain_id(chain_id)
-        assert "chain_id is not recognized" in str(ex.value)
+    result = short_name_from_chain_id(chain_id)
+    assert isinstance(result, str)
+    assert result == expected_short_name
 
 
 @pytest.mark.parametrize(
     ("chain_id", "expected_network"),
     [
         (1, Network(1, "Ethereum Mainnet", "eth", ChainId.ETH)),
-        (249, None),
         (2, Network(2, "Expanse Network", "exp", ChainId.EXP)),
         (42, Network(42, "LUKSO Mainnet", "lukso", ChainId.LUKSO)),
         (43, Network(43, "Darwinia Pangolin Testnet", "pangolin", ChainId.PANGOLIN)),
     ],
 )
 def test_network_from_chain_id(chain_id, expected_network):
-    if expected_network is not None:
-        result = network_from_chain_id(chain_id)
-        assert isinstance(result, Network)
-        assert result == expected_network
-    else:
-        with pytest.raises(ValidationError) as ex:
-            network_from_chain_id(chain_id)
-        assert "chain_id is not recognized" in str(ex.value)
+    result = network_from_chain_id(chain_id)
+    assert isinstance(result, Network)
+    assert result == expected_network
+
+
+@pytest.mark.parametrize("invalid_chain_id", (222**222, 333**333, 444**444))
+def test_from_chain_id_utility_methods_with_invalid_chain_id(invalid_chain_id):
+    error_message = f"chain_id is not recognized: {invalid_chain_id}"
+
+    # test network_from_chain_id
+    with pytest.raises(ValidationError, match=error_message):
+        network_from_chain_id(invalid_chain_id)
+
+    # test short_name_from_chain_id
+    with pytest.raises(ValidationError, match=error_message):
+        short_name_from_chain_id(invalid_chain_id)
+
+    # test name_from_chain_id
+    with pytest.raises(ValidationError, match=error_message):
+        name_from_chain_id(invalid_chain_id)
