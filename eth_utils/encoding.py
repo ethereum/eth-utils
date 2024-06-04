e0a381a39298e315a7d8ce19469f3d6ea736ec5a
Stuart Reed <stuart.reed@ethereum.org>
2024-06-04 12:24:05 -0600

    Revert codec changes

--- eth_utils/encoding.py
+++ eth_utils/encoding.py
@@ -1,27 +1,6 @@
-from typing import (
-    TYPE_CHECKING,
-)
-
-if TYPE_CHECKING:
-    from eth_abi.abi import (
-        ABICodec,
-    )
-
-
 def int_to_big_endian(value: int) -> bytes:
     return value.to_bytes((value.bit_length() + 7) // 8 or 1, "big")
 
 
 def big_endian_to_int(value: bytes) -> int:
     return int.from_bytes(value, "big")
-
-
-def get_default_codec() -> "ABICodec":
-    from eth_abi.abi import (
-        ABICodec,
-    )
-    from eth_abi.registry import (
-        registry as default_registry,
-    )
-
-    return ABICodec(default_registry)
