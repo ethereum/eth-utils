98ea85a51cf91223875b02435560fa05bbae992d
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-06-03 17:31:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ with open("./README.md") as readme:
 setup(
     name="faster-eth-utils",
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version="5.3.0",
+    version="5.3.01",
     description=(
         """A fork of eth-utils: Common utility functions for python code that interacts with Ethereum, implemented in C"""
     ),
