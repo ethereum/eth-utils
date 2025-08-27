2411b1f9a4d29feceaf8ede8f7b67ac0196ca3bf
kclowes <kclowes@users.noreply.github.com>
2025-08-27 10:20:39 -0600

    Bump version: 5.3.0 → 5.3.1

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open("./README.md") as readme:
 setup(
     name="eth-utils",
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version="5.3.0",
+    version="5.3.1",
     description=(
         """eth-utils: Common utility functions for python code that interacts with Ethereum"""
     ),
