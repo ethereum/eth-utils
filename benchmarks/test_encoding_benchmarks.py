661d4949375be259afdb7463a6b8371106b1af66
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-06 15:28:21 -0400

    feat(benchmark): parametrize encoding benchmarks (#67)
    * feat(benchmark): parametrize encoding benchmarks
    
    * ids
    
    * fix
    
    * chore: compile C files for source control
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- benchmarks/test_encoding_benchmarks.py
+++ benchmarks/test_encoding_benchmarks.py
@@ -1,4 +1,5 @@
-from typing import Any, Callable
+# mypy: disable-error-code=misc
+from typing import Any, Callable, Union
 
 import eth_utils
 import pytest
@@ -6,25 +7,56 @@ from pytest_codspeed import BenchmarkFixture
 
 import faster_eth_utils
 
-
 def _batch(i: int, fn: Callable[..., Any], *inputs: Any) -> None:
     for _ in range(i):
         fn(*inputs)
 
-@pytest.mark.benchmark(group="int_to_big_endian")
-def test_int_to_big_endian(benchmark: BenchmarkFixture) -> None:
-    benchmark(_batch, 100, eth_utils.int_to_big_endian, 123456789)
+int_to_be_cases = [
+    0,
+    1,
+    255,
+    256,
+    2**256 - 1,
+]
+int_to_be_ids = [
+    "zero",
+    "one",
+    "255",
+    "256",
+    "max",
+]
+
+be_to_int_cases = [
+    b"",
+    b"\x01",
+    b"\xff",
+    b"\x00\x01",
+    b"\xff" * 32,
+]
+be_to_int_ids = [
+    "empty-bytes",
+    "one-byte",
+    "ff-byte",
+    "two-bytes",
+    "32-ff-bytes",
+]
 
 @pytest.mark.benchmark(group="int_to_big_endian")
-def test_faster_int_to_big_endian(benchmark: BenchmarkFixture) -> None:
-    benchmark(_batch, 100, faster_eth_utils.int_to_big_endian, 123456789)
+@pytest.mark.parametrize("value", int_to_be_cases, ids=int_to_be_ids)
+def test_int_to_big_endian(benchmark: BenchmarkFixture, value: int) -> None:
+    benchmark(_batch, 10, eth_utils.int_to_big_endian, value)
+
+@pytest.mark.benchmark(group="int_to_big_endian")
+@pytest.mark.parametrize("value", int_to_be_cases, ids=int_to_be_ids)
+def test_faster_int_to_big_endian(benchmark: BenchmarkFixture, value: int) -> None:
+    benchmark(_batch, 10, faster_eth_utils.int_to_big_endian, value)
 
 @pytest.mark.benchmark(group="big_endian_to_int")
-def test_big_endian_to_int(benchmark: BenchmarkFixture) -> None:
-    value = (123456789).to_bytes(8, "big")
-    benchmark(_batch, 100, eth_utils.big_endian_to_int, value)
+@pytest.mark.parametrize("value", be_to_int_cases, ids=be_to_int_ids)
+def test_big_endian_to_int(benchmark: BenchmarkFixture, value: Union[bytes, str]) -> None:
+    benchmark(_batch, 10, eth_utils.big_endian_to_int, value)
 
 @pytest.mark.benchmark(group="big_endian_to_int")
-def test_faster_big_endian_to_int(benchmark: BenchmarkFixture) -> None:
-    value = (123456789).to_bytes(8, "big")
-    benchmark(_batch, 100, faster_eth_utils.big_endian_to_int, value)
+@pytest.mark.parametrize("value", be_to_int_cases, ids=be_to_int_ids)
+def test_faster_big_endian_to_int(benchmark: BenchmarkFixture, value: Union[bytes, str]) -> None:
+    benchmark(_batch, 10, faster_eth_utils.big_endian_to_int, value)
