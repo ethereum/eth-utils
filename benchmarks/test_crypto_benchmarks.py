65d3ee1d2becab58389cedb7baab778d2c44f4db
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-06 15:03:08 -0400

    feat(benchmark): parametrize crypto benchmarks (#61)
    * feat(benchmark): parametrize crypto benchmarks
    
    * fix
    
    * chore: compile C files for source control
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- benchmarks/test_crypto_benchmarks.py
+++ benchmarks/test_crypto_benchmarks.py
@@ -1,4 +1,5 @@
-from typing import Any, Callable
+# mypy: disable-error-code=misc
+from typing import Any, Callable, Dict
 
 import eth_utils
 import pytest
@@ -6,19 +7,31 @@ from pytest_codspeed import BenchmarkFixture
 
 import faster_eth_utils
 
-
-def _batch(i: int, fn: Callable[..., Any], *inputs: Any) -> None:
+def _batch(i: int, fn: Callable[..., Any], **kwargs: Any) -> None:
     for _ in range(i):
-        fn(*inputs)
+        fn(**kwargs)
 
+cases = [
+    {"primitive": b"hello world"},         # bytes
+    {"primitive": 123456789},              # int
+    {"primitive": True},                   # bool
+    {"hexstr": "0x68656c6c6f"},            # hexstr
+    {"text": "hello world"},               # text
+]
+ids = [
+    "bytes",
+    "int",
+    "bool",
+    "hexstr",
+    "text",
+]
 
 @pytest.mark.benchmark(group="keccak")
-def test_keccak(benchmark: BenchmarkFixture) -> None:
-    data = b"hello world"
-    benchmark(_batch, 100, eth_utils.keccak, data)
-
+@pytest.mark.parametrize("kwargs", cases, ids=ids)
+def test_keccak(benchmark: BenchmarkFixture, kwargs: Dict[str, Any]) -> None:
+    benchmark(_batch, 10, eth_utils.keccak, **kwargs)
 
 @pytest.mark.benchmark(group="keccak")
-def test_faster_keccak(benchmark: BenchmarkFixture) -> None:
-    data = b"hello world"
-    benchmark(_batch, 100, faster_eth_utils.keccak, data)
+@pytest.mark.parametrize("kwargs", cases, ids=ids)
+def test_faster_keccak(benchmark: BenchmarkFixture, kwargs: Dict[str, Any]) -> None:
+    benchmark(_batch, 10, faster_eth_utils.keccak, **kwargs)
